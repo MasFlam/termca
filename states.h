edd59435555eef7c0708b27727def89f6a09a3ee
MasFlam <masflam@masflam.com>
2020-12-15 17:29:35 +0100

    change the default automaton to GoL

--- states.h
+++ states.h
@@ -1,3 +1,3 @@
 // Change this include to the template you want to see.
 // Don't forget to change this transition.inc accordingly too!
-#include "automata/neumann_states.h"
+#include "automata/gol_states.h"
